ad740e5566c9b748d6712ee16b078e8c4f53b075
markilha@gmail.com <markilha@gmail.com>
2024-08-03 19:26:22 -0300

    Subiu Projeto

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,98 @@
+import React from "react";
+import { FaInstagram, FaWhatsapp, FaYoutube } from "react-icons/fa";
+import { TbWorldWww } from "react-icons/tb";
+import { motion } from "framer-motion";
+
+const Footer = () => {
+  return (
+    <footer className="py-28 bg-[#f7f7f7]">
+      <motion.div
+        initial={{ opacity: 0, y: 50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        className="container"
+      >
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-14 md:gap-4">
+          {/* first section */}
+          <div className="space-y-4 max-w-[300px]">
+            <h1 className="text-2xl font-bold">A jornada de codificação</h1>
+            <p className="text-dark2">
+            TCJ é uma plataforma dedicada a capacitar desenvolvedores aspirantes. De tutoriais para iniciantes a conceitos avançados de programação, oferecemos uma experiência de aprendizado abrangente, projetada para ajudar você a dominar habilidades de codificação, construir projetos e lançar sua carreira em tecnologia.
+            </p>
+          </div>
+          {/* second section */}
+          <div className="grid grid-cols-2 gap-10">
+            <div className="space-y-4">
+              <h1 className="text-2xl font-bold">Cursos</h1>
+              <div className="text-dark2">
+                <ul className="space-y-2 text-lg">
+                  <li className="cursor-pointer hover:text-secondary duration-200">
+                    Desenvolvimento Web
+                  </li>
+                  <li className="cursor-pointer hover:text-secondary duration-200">
+                    Desenvolvimento de software
+                  </li>
+                  <li className="cursor-pointer hover:text-secondary duration-200">
+                    Desenvolvimento de aplicativos
+                  </li>
+                  <li className="cursor-pointer hover:text-secondary duration-200">
+                    E-learning
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div className="space-y-4">
+              <h1 className="text-2xl font-bold">Links</h1>
+              <div className="text-dark2">
+                <ul className="space-y-2 text-lg">
+                  <li className="cursor-pointer hover:text-secondary duration-200">
+                    Home
+                  </li>
+                  <li className="cursor-pointer hover:text-secondary duration-200">
+                    Serviços
+                  </li>
+                  <li className="cursor-pointer hover:text-secondary duration-200">
+                    Sobre
+                  </li>
+                  <li className="cursor-pointer hover:text-secondary duration-200">
+                    Contato
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          {/* third section */}
+          <div className="space-y-4 max-w-[300px]">
+            <h1 className="text-2xl font-bold">Entrar em contato</h1>
+            <div className="flex items-center">
+              <input
+                type="text"
+                placeholder="Digite o seu email"
+                className="p-3 rounded-s-xl bg-white w-full py-4 focus:ring-0 focus:outline-none placeholder:text-dark2"
+              />
+              <button className="bg-primary text-white font-semibold py-4 px-6 rounded-e-xl">
+                Ir
+              </button>
+            </div>
+            {/* social icons */}
+            <div className="flex space-x-6 py-3">
+              <a href="https://chat.whatsapp.com/FQSKgJ5f1eIAhlyF5sVym0">
+                <FaWhatsapp className="cursor-pointer hover:text-primary hover:scale-105 duration-200" />
+              </a>
+              <a href="https://www.instagram.com/the.coding.journey/">
+                <FaInstagram className="cursor-pointer hover:text-primary hover:scale-105 duration-200" />
+              </a>
+              <a href="https://thecodingjourney.com/">
+                <TbWorldWww className="cursor-pointer hover:text-primary hover:scale-105 duration-200" />
+              </a>
+              <a href="https://www.youtube.com/@TheCodingJourney">
+                <FaYoutube className="cursor-pointer hover:text-primary hover:scale-105 duration-200" />
+              </a>
+            </div>
+          </div>
+        </div>
+      </motion.div>
+    </footer>
+  );
+};
+
+export default Footer;
